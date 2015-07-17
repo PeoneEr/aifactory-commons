ad7719ba9acbc17ee9838cb7905e602fd849dc62
PeoneEr <peoneerko@yandex.ru>
2015-07-17 14:33:37 +0600

    add generator and requirments

--- aifactory-commons.gemspec
+++ aifactory-commons.gemspec
@@ -29,4 +29,8 @@ Gem::Specification.new do |spec|
 
   spec.add_development_dependency "bundler", "~> 1.10"
   spec.add_development_dependency "rake", "~> 10.0"
+
+  spec.add_runtime_dependency 'hirb'
+  spec.add_runtime_dependency 'annotate'
+  spec.add_runtime_dependency 'configliere'
 end
---
+++ lib/aifactory/generators/commons/settings_generator.rb
@@ -0,0 +1,14 @@
+module Commons
+  module Generators
+    class SettingsGenerator < Rails::Generators::Base
+
+      desc 'Generate initializer with settings'
+      source_root File.expand_path("../templates", __FILE__)
+
+      def copy_initializer_file
+        copy_file "_read_settings.rb", "config/initializers/_read_settings.rb"
+      end
+
+    end
+  end
+end
